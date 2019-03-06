0d6316cc9bb1f11f6d33e7d4b6374b9b1aaec119
Ábel Kalamár <abel.kalamar@gmail.com>
2019-03-06 17:07:47 +0100

    add paths, create registration component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,12 +9,14 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
+import { RegistrationComponent } from './components/registration/registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SideBarComponent,
-    NavbarComponent
+    NavbarComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
