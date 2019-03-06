cc70386157bd43ebec21253aa85493e3afbdada3
Balazs Hegedus <hegedusb93@gmail.com>
2019-03-06 18:16:39 +0100

    feat(Uniware backend) postSubjects method updated, header error handling removed

--- backend/controllers/subject.controller.js
+++ backend/controllers/subject.controller.js
@@ -9,11 +9,6 @@ const getSubjects = (req, res, next) => {
 
 const postSubjects = (req, res, next) => {
   const { name, exp } = req.body;
-  if (name === '' || exp === 0) {
-    return res.status(400).json({
-      error: 'Missing header or missing parameter(s)'
-    });
-  }
   const newSubject = new subjectModel({
     name,
     exp,
