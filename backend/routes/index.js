597bd8e788154d2daefbeb3b83ce603a49113aef
Balazs Hegedus <hegedusb93@gmail.com>
2019-03-07 02:03:56 +0100

    feat(Uniware backend) index.js updated, unneccessary spacings removed

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,7 +4,6 @@ const userController = require('../controllers/user.controller');
 const auth = require('../middlewares/authorization');
 const multer = require('multer');
 
-
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, 'static');
@@ -19,13 +18,11 @@ const upload = multer({ storage: storage });
 const router = express.Router();
 router.use(express.json());
 
-
 router.get('/subject', auth.authorizeUser, subjectController.getSubjects),
 router.post('/subject', auth.authorizeUser, subjectController.postSubjects),
 router.get('/subject/search', auth.authorizeUser, subjectController.searchSubjects ),
 router.post('/subject/file', auth.authorizeUser, upload.single('path'), subjectController.postFiles),
 router.get('/subject/file', auth.authorizeUser, subjectController.downloadFiles),
-
 
 router.post('/register', userController.registerController);
 
